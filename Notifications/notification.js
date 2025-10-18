1a52958aa306172eae3e727440d9d4f51a2afba1
shinubo28always <shinubo28always@gmail.com>
2025-10-18 17:32:32 +0600

    Update notification.js

--- Notifications/notification.js
+++ Notifications/notification.js
@@ -1,6 +1,14 @@
 // === Notifications Array (Je text add korbe, sheta show hobe) ===
 const notifications = [
-    "Pta nii kon bahenchod ka najar lag gye🤬"
+    "Pta nii kon bahenchod ka najar lag gye🤬",
+    "ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ
+ "ᴀʟʟ ᴀɴɪᴍᴇs ɪɴ ʜɪɴᴅɪ ᴅᴜʙ ᴄʜᴀɴɴᴇʟ" 
+
+ᴀʟʟ ɴᴇᴡ ʜɪɴᴅɪ ᴅᴜʙ ᴀɴɪᴍᴇs ʙʏ ᴄʀᴜɴᴄʜʏʀᴏʟʟ ᴏʀ ᴀɴʏ ᴏᴛʜᴇʀ ᴘʟᴀᴛғᴏʀᴍs ᴡɪʟʟ ʙᴇ ᴜᴘʟᴏᴀᴅᴇᴅ ʜᴇʀᴇ.
+ᴛʜᴇ ʙᴇsᴛ ᴀɴɪᴍᴇ sᴜᴘᴘᴏʀᴛ ᴛʜᴇᴍ...
+
+Rᴇǫᴜᴇsᴛ Aɴɪᴍᴇ:- @AniReal_Chat_Group_Asia
+                             :- @AniReal_Support"
 ];
 
 // Home page badge
